dbaa340c54e9225f6b6eb1c6dd13a8d18eee29d0
hanlai.li(李晗来) <hanlai.li@transsion.com>
2020-07-17 11:02:47 +0800

    加入创建数据库表等配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    //市的名字
+    private String cityName;
+    //市的代号
+    private int cityCode;
+    //所属省份id值
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    //省的名字
+    private String provinceName;
+    //省的代号
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
