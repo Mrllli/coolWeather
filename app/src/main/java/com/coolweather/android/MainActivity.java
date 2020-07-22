bcf51b854d446040f4dad083ea7b5d69ffed8113
hanlai.li <HANLAI.LI@TRANSSION.COM>
2020-07-22 11:16:32 +0800

    更新背景，图标

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,19 +1,19 @@
 package com.coolweather.android;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView bingPicImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
     }
+
 
 }
--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,17 +1,21 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -46,6 +50,13 @@ public class WeatherActivity extends AppCompatActivity {
     //backgroundImage
     private ImageView bingPicImg;
 
+    //backButton
+    private Button backButton;
+
+    //backgroundAlpha
+    private View v;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,8 +73,15 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText = (TextView) findViewById(R.id.comfort_text);
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
-
+        //背景图片
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
+
+        //回退按钮
+        backButton = (Button) findViewById(R.id.bav_button);
+
+        v = (View) findViewById(R.id.backGround_weather);
+        v.getBackground().setAlpha(190);
+
 //        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 //        String weatherString = prefs.getString("weather",null);
 //        Log.d(TAG, "onCreate: " + weatherString);
@@ -81,11 +99,11 @@ public class WeatherActivity extends AppCompatActivity {
         String weatherId = getIntent().getStringExtra("weather_id");
         requestWeather(weatherId);
 
-
         //loadImage
         loadBingPic();
 
-
+        //设置返回按钮
+        setResponseButton();
     }
 
     public void requestWeather(final String weatherId){
@@ -199,6 +217,17 @@ public class WeatherActivity extends AppCompatActivity {
                         Glide.with(WeatherActivity.this).load(bingPicNew).into(bingPicImg);
                     }
                 });
+            }
+        });
+    }
+    //返回键
+    private void setResponseButton(){
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WeatherActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
